ed19392fcc2ee7001de7d44b3bc3bb21f03d2170
Manel Clos <manelclos@gmail.com>
2017-12-29 11:17:51 +0100

    export components as library

--- config/index.js
+++ config/index.js
@@ -47,7 +47,16 @@ module.exports = {
     // just be aware of this issue when enabling this option.
     cssSourceMap: false,
   },
-
+  lib: {
+    env: require('./prod.env'),
+    assetsRoot: path.resolve(__dirname, '../dist'),
+    assetsSubDirectory: 'lib',
+    assetsPublicPath: '/',
+    productionSourceMap: true,
+    productionGzip: false,
+    productionGzipExtensions: ['js', 'css'],
+    bundleAnalyzerReport: process.env.npm_config_report
+  },
   build: {
     // Template for index.html
     index: path.resolve(__dirname, '../dist/index.html'),
---
+++ src/lib.js
@@ -0,0 +1,19 @@
+import AppGeoportal from '@/App'
+import AppHeader from '@/components/AppHeader'
+import AppFooter from '@/components/AppFooter'
+import GeoportalMap from '@/components/GeoportalMap'
+import Sidebar from '@/components/Sidebar'
+import GeoportalPanel from '@/components/GeoportalPanel'
+import PlacePanel from '@/components/PlacePanel'
+import SearchPanel from '@/components/SearchPanel'
+
+export {
+  AppGeoportal,
+  AppHeader,
+  AppFooter,
+  GeoportalMap,
+  Sidebar,
+  GeoportalPanel,
+  PlacePanel,
+  SearchPanel
+}
