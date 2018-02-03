2c89a41eb097a02cf1f202a727eba2eecd5ed59d
Manel Clos <manelclos@gmail.com>
2018-02-03 09:19:59 +0100

    add OpenTopoMap

--- src/config.js
+++ src/config.js
@@ -24,6 +24,14 @@ export default {
     {
       default: false,
       type: 'tilelayer',
+      name: 'OpenTopoMap',
+      url: 'https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png',
+      attribution: 'Map data: &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)',
+      maxZoom: 19
+    },
+    {
+      default: false,
+      type: 'tilelayer',
       name: 'Stamen TonerLite',
       url: 'https://stamen-tiles-{s}.a.ssl.fastly.net/toner-lite/{z}/{x}/{y}.{ext}',
       attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
