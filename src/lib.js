c0e912e9759d466594add6703b0f2095167c9a24
Manel Clos <manelclos@gmail.com>
2018-04-20 13:11:20 +0200

    export MeasurePanel

--- src/lib.js
+++ src/lib.js
@@ -6,6 +6,7 @@ import CatalogPanel from '@/components/CatalogPanel'
 import CatalogResult from '@/components/CatalogResult'
 import Sidebar from '@/components/Sidebar'
 import GeoportalPanel from '@/components/GeoportalPanel'
+import MeasurePanel from '@/components/MeasurePanel'
 import PlacePanel from '@/components/PlacePanel'
 import SearchPanel from '@/components/SearchPanel'
 import QueryOnClick from '@/components/QueryOnClick'
@@ -21,6 +22,7 @@ export {
   GeoportalMap,
   Sidebar,
   GeoportalPanel,
+  MeasurePanel,
   PlacePanel,
   SearchPanel,
   QueryOnClick,
