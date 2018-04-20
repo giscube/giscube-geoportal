5fa3060840321a3c2f4a7d96ed7cd01636ff1380
Manel Clos <manelclos@gmail.com>
2018-04-20 11:34:16 +0200

    use tabs for content

--- src/components/FeatureInfoPopup.vue
+++ src/components/FeatureInfoPopup.vue
@@ -1,18 +1,23 @@
 <template>
   <div class='popup'>
-    <div v-for='layer in results' class='layer'>
-      <div class='title'>{{ layer.attributes.name }}</div>
-      <div v-for='feature in layer.elements' class='feature'>
-        <table class="table table-striped table-hover">
-          <tbody>
-            <tr v-for='attr in feature.elements' class='attr'>
-              <th>{{ attr.attributes.name }}</th>
-              <td>{{ attr.attributes.value }}</td>
-            </tr>
-          </tbody>
-        </table>
+    <el-tabs @tab-click="handleClick">
+      <div v-for='layer in results' class='layer'>
+        <el-tab-pane class='popup-content'>
+          <span v-if="layer.elements.length === 1" slot="label">{{ layer.attributes.name }}</span>
+          <span v-if="layer.elements.length > 1" slot="label">{{ layer.attributes.name }} ({{ layer.elements.length }})</span>
+          <div v-for='feature in layer.elements' class='feature'>
+            <table class="table table-striped table-hover">
+              <tbody>
+                <tr v-for='attr in feature.elements' class='attr'>
+                  <th>{{ attr.attributes.name }}</th>
+                  <td>{{ attr.attributes.value }}</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </el-tab-pane>
       </div>
-    </div>
+    </el-tabs>
   </div>
 </template>
 
@@ -29,6 +34,8 @@ export default {
 
 <style lang="scss">
 .popup {
+}
+.popup-content {
   max-height: 300px;
   overflow: auto;
 }
@@ -41,10 +48,18 @@ export default {
 
 .layer .feature {
   font-weight: bold;
+  border-bottom: 3px solid #aaa;
+  margin-bottom: 20px;
+}
+
+.layer .feature:last-child {
+  border-bottom: 0px;
+  margin-bottom: 0px;
 }
 
 .table > tbody > tr > th, .table > tbody > tr > td {
-  padding: 3px
+  padding: 3px;
+  margin-bottom: 0px;
 }
 
 </style>
