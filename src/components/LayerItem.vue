3da4c57dd4f3276de26340345b78f543cd5b121b
Manel Clos <manelclos@gmail.com>
2019-03-15 08:10:23 +0100

    style fixes

--- src/components/LayerItem.vue
+++ src/components/LayerItem.vue
@@ -1,18 +1,26 @@
 <template>
-  <li>
+  <li class="layer-item">
     <div>
       <div class="drag-handle" style="display: flex;">
         <q-checkbox
           v-model="layer.visible"
           :label="layer.name"
+          color="black"
           style="flex-grow: 1;"
           @input="$emit('toggle-layer', { options: layer, visible: $event })"
         />
         <q-btn
           flat
           round
+          @click="$emit('remove-layer', layer)"
+          class="desktop-only"
+        ><q-icon name="delete" size="20px" /></q-btn>
+        <q-btn
+          flat
+          round
           icon="delete"
           @click="$emit('remove-layer', layer)"
+          class="desktop-hide"
         />
         <q-btn
           flat
@@ -49,12 +57,37 @@ export default {
 </script>
 
 <style lang="scss">
-.giscube-layers-control {
+.layer-item {
+  .desktop-only {
+    color: transparent;
+  }
+  &:hover .desktop-only {
+    color: black;
+  }
+
   .layerinfo {
     padding: 10px 20px;
     width: 100%;
     background-color: #eee;
     border-bottom: 1px dashed #ccc;
+  }
+  .q-checkbox__inner {
+    width: 34px;
+    min-width: 34px;
+    height: 34px;
+  }
+  .q-checkbox__bg {
+    left: 10px;
+    top: 10px;
+  }
+  .q-checkbox__label {
+    font-size: 12px;
+  }
+  .q-btn--round {
+    height: 38px;
+    min-height: 38px;
+    width: 38px;
+    min-width: 38px;
   }
 }
 </style>
