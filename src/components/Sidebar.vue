076c484a06e5747e2737f5253ba4a50f2f997d55
Manel Clos <manelclos@gmail.com>
2019-03-19 18:03:52 +0100

    sidebar resizing

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -12,6 +12,17 @@
         color="white"
         size="35px"
         /></a>
+    <a v-if="sidebarVisible"
+       @click.prevent="onToggleWidthClick"
+       @mousedown="onToggleWidthMousedown"
+       @touchstart="onToggleWidthMousedown"
+       @touchend="onToggleWidthClick"
+       class="sidebar-close sidebar-toggle-width"
+       ><q-icon
+        name="drag_indicator"
+        color="white"
+        size="35px"
+        /></a>
   </q-drawer>
 </template>
 
@@ -24,7 +35,9 @@ export default {
   props: ['map', 'visible', 'geoportalMap'],
   data () {
     return {
-      width: 300
+      resizeDiff: 0,
+      width: 300,
+      widthPercentage: 30
     }
   },
   computed: {
@@ -45,10 +58,26 @@ export default {
     'map': 'mapChanged'
   },
   mounted () {
+    document.documentElement.addEventListener('mousemove', this.onToggleWidthMousemove)
+    document.documentElement.addEventListener('mouseup', this.onToggleWidthMouseup)
+    document.documentElement.addEventListener('mouseleave', this.onToggleWidthMouseup)
+
+    document.documentElement.addEventListener('touchmove', this.onToggleWidthMousemove, true)
+    document.documentElement.addEventListener('touchend touchcancel', this.onToggleWidthMouseup, true)
+
     this.$nextTick(() => {
       this.onResize()
       this.$store.commit('setSidebarVisible', true)
     })
+  },
+  beforeDestroy () {
+    document.documentElement.removeEventListener('mousemove', this.onToggleWidthMousemove)
+    document.documentElement.removeEventListener('mouseup', this.onToggleWidthMouseup)
+    document.documentElement.removeEventListener('mouseleave', this.onToggleWidthMouseup)
+
+    document.documentElement.removeEventListener('touchmove', this.onToggleWidthMousemove, true)
+    document.documentElement.removeEventListener('touchend touchcancel', this.onToggleWidthMouseup, true)
+    document.documentElement.removeEventListener('touchstart', this.onToggleWidthMouseup, true)
   },
   methods: {
     getMapInfo () {
@@ -81,12 +110,74 @@ export default {
     },
     onResize () {
       debounce(() => {
-        let width = (this.mapSize && this.mapSize.width / 3) || 300
-        this.width = Math.max(300, Math.min(400, width))
+        let width
+        let widthPercentage = this.widthPercentage
+        let rightMargin = 150
+        if (this.mapSize) {
+          if (this.mapSize.width < 600) {
+            // sidebar breakpoint set at 600px
+            widthPercentage = 95
+            rightMargin = 25
+          }
+          width = this.mapSize.width * widthPercentage / 100
+          width = Math.min(this.mapSize.width - rightMargin, width)
+          width = Math.max(300, width)
+        } else {
+          width = 300
+        }
+        // this.width = Math.max(this.widthPercentage * 10, Math.min((this.widthPercentage + 10) * 100, width))
+        this.width = width
       }, 300, true)()
     },
     onToggleClick () {
       this.$store.commit('setSidebarVisible', !this.sidebarVisible)
+    },
+    onToggleWidthClick () {
+      if (this.resized) {
+        return
+      }
+
+      const changed = [45, 60, 75].some(width => {
+        if (this.widthPercentage < (width - 5)) {
+          this.widthPercentage = width
+          return true
+        }
+      })
+      if (!changed) {
+        this.widthPercentage = 30
+      }
+      this.onResize()
+    },
+    onToggleWidthMousedown (event) {
+      event.preventDefault()
+      event.stopPropagation()
+      this.resizing = true
+      this.resized = false
+      const mouseX = event.pageX || event.touches[0].pageX
+      this.resizeDiff = mouseX - this.width
+    },
+    onToggleWidthMousemove (event) {
+      if (!this.resizing) {
+        return
+      }
+      event.stopPropagation()
+      const mouseX = event.pageX || event.touches[0].pageX
+      if (event.movementX) {
+        this.resized = this.resized || (Math.abs(event.movementX) > 1) || (Math.abs(event.movementY) > 1)
+      } else {
+        this.resized = true
+      }
+      const widthPercentage = (mouseX - this.resizeDiff) / this.mapSize.width * 100
+      this.widthPercentage = Math.min(90, Math.max(30, widthPercentage))
+      this.onResize()
+    },
+    onToggleWidthMouseup (event) {
+      if (this.resizing) {
+        event.preventDefault()
+        event.stopPropagation()
+        this.resizing = false
+        this.resizeDiff = 0
+      }
     },
     setWide (isWide) {
       let klass = this.sidebar._container.className
@@ -151,6 +242,23 @@ aside.q-drawer--left {
 }
 .q-drawer--mobile .sidebar-close {
   top: 59px;
+}
+
+.sidebar-toggle-width {
+  left: calc(100% + 30px);
+  border-radius: 4px;
+  width: 30px;
+}
+.sidebar-toggle-width i {
+  margin-left: -11px;
+  margin-top: -7px;
+}
+
+// Same as breakpoint
+@media (max-width: 600px) {
+  .sidebar-toggle-width {
+    display: none;
+  }
 }
 
 .panel-content {
