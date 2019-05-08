690061f573eb789e3f3b2a543036aa7efccbd162
Martí Angelats i Ribera <ma@microdisseny.com>
2019-05-08 11:10:34 +0200

    Make SQLChoices select table searchable and sortable

--- src/lib/field/widgets/form/SqlChoices.vue
+++ src/lib/field/widgets/form/SqlChoices.vue
@@ -18,57 +18,68 @@
 
     <q-dialog
       ref="dialog"
-      maximized
+      :maximized="!$q.screen.gt.sm"
+      :full-width="$q.screen.gt.sm"
+      :full-height="$q.screen.gt.sm"
     >
       <q-card>
-        <q-card-section style="display: flex;">
-          <q-markup-table style="margin-left:auto; margin-right: auto;">
-            <thead>
-              <tr>
-                <th :colspan="field.headers.length">
-                  <div class="full-width row">
-                    <div class="text-h6">{{ field.label }}</div>
-                    <q-space />
-                    <q-btn
-                      label="Close"
-                      flat
-                      @click="$refs.dialog.hide()"
-                    />
-                  </div>
-                </th>
-              </tr>
-              <tr>
-                <th
-                v-for="header in field.headers"
-                :key="'header-' + header"
-                >
-                {{ header }}
-              </th>
-            </tr>
-            </thead>
-            <tbody>
-              <tr
-                v-for="values in field.valuesList"
-                :key="'row-' + values[0]"
-                @click="select(values)"
-              >
+        <q-card-section>
+          <q-table
+            :title="field.label"
+            :data="data"
+            :columns="columns"
+            :row-key="field.headers[0]"
+            :filter="filter"
+            :pagination.sync="pagination"
+            :flat="$q.screen.gt.sm"
+            hide-bottom
+          >
+            <template v-slot:top="props">
+              <div class="q-table__title">{{ field.label }}</div>
+              <q-space />
+              <q-input borderless dense debounce="300" v-model="filter" :placeholder="$t('actions.search') | capitalize">
+                <template v-slot:before>
+                  <q-icon name="search" />
+                </template>
+              </q-input>
+              <q-space />
+              <q-btn
+                label="Close"
+                color="primary"
+                flat
+                @click="$refs.dialog.hide()"
+              />
+            </template>
+            <template v-slot:top-row="props" v-if="current">
+              <tr class="sqlchoices-current">
                 <td
-                  v-for="(value, i) in values"
-                  :key="'row-' + values[0] + '-' + i"
-                >
-                  {{ value }}
-                </td>
+                  v-for="col in props.cols"
+                  :key="'top-' + col.name"
+                  :style="{
+                    'text-align': col.align
+                  }"
+                >{{ col.field(current) }}</td>
               </tr>
-            </tbody>
-          </q-markup-table>
+            </template>
+            <template v-slot:body-cell="props">
+              <q-td
+                :props="props"
+                class="cursor-pointer"
+                @click.native="select(props.row)"
+              >
+                {{ props.value }}
+              </q-td>
+            </template>
+          </q-table>
         </q-card-section>
       </q-card>
     </q-dialog>
+
   </div>
 </template>
 
 <script>
-import { QBtn, QCard, QCardSection, QDialog, QField, QMarkupTable, QSpace } from 'quasar'
+import { QBtn, QCard, QCardSection, QDialog, QField, QIcon, QInput, QSpace, QTable, QTd } from 'quasar'
 
 import MultiResult from '../../../MultiResult.js'
 
@@ -80,16 +91,27 @@ export default {
     QCardSection,
     QDialog,
     QField,
-    QMarkupTable,
-    QSpace
+    QIcon,
+    QInput,
+    QSpace,
+    QTable,
+    QTd
+  },
+  data () {
+    return {
+      filter: '',
+      pagination: { rowsPerPage: 0 }
+    }
   },
   computed: {
-    v () {
+    current () {
       if (MultiResult.is(this.value)) {
         return null
       }
-      const result = this.valid(this.value) && this.field.valuesDict[this.value]
-      return result ? result[1] : this.value
+      return this.valid(this.value) && this.field.valuesDict[this.value]
+    },
+    v () {
+      return this.current ? this.current[1] : this.value
     },
     hint () {
       if (MultiResult.is(this.value)) {
@@ -99,11 +121,24 @@ export default {
         }).join(', ')
       }
       return undefined
+    },
+
+    columns () {
+      return this.field.headers.map((header, i) => ({
+        name: header,
+        label: header,
+        field: row => row[i],
+        sortable: true,
+        sort: this.compare
+      }))
+    },
+    data () {
+      return this.field.valuesList
     }
   },
   methods: {
-    select (values) {
-      this.$emit('input', values[0])
+    select (row) {
+      this.$emit('input', row[0])
 
       this.$nextTick(_ => this.$refs.dialog.hide())
     },
@@ -114,7 +149,27 @@ export default {
     },
     valid (value) {
       return value !== null && value !== undefined
+    },
+    compare (a, b) {
+      const ta = typeof a
+      const tb = typeof b
+      if (ta !== tb) {
+        return (ta).localeCompare(tb)
+      } else if (ta === 'number') {
+        return a - b
+      } else if (ta === 'string') {
+        return (a).localeCompare(b)
+      } else {
+        return (a.toString()).localeCompare(b.toString())
+      }
     }
   }
 }
 </script>
+
+<style lang="stylus">
+@import '~quasar-variables'
+
+tr.sqlchoices-current
+  background-color: $grey-5 !important // important to prevent :hover to change its style
+</style>
