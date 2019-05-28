3cade071950fd33dc66ecdf50e5cde4a6ba15506
Manel Clos <manelclos@gmail.com>
2019-05-28 15:06:57 +0200

    display correct value in popup

--- src/lib/field/PkField.js
+++ src/lib/field/PkField.js
@@ -3,9 +3,7 @@ import Vue from 'vue'
 import Field from './Field'
 
 export default class PkField extends Field {
-  tableValue (data) {
-    const value = this.getValue(data)
-
+  val2str (value) {
     if (typeof value === 'string' && value.startsWith('__new')) {
       const n = Vue.prototype.$t('states.new')
       return `(${n})`
