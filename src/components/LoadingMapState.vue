550d5de11f066a3733b5e6d4cbb5b2857993b0fb
Martí Angelats i Ribera <ma@microdisseny.com>
2019-07-08 17:31:47 +0200

    Add loading data feedback over map

---
+++ src/components/LoadingMapState.vue
@@ -0,0 +1,40 @@
+<template>
+  <div
+    v-show="loading"
+    class="loader"
+    :style="{
+      left: leftDrawerSize + 'px'
+    }"
+  >
+    <div class="absolute-center" style="width: 20vmin; height: 20vmin">
+      <q-spinner size="100%" />
+    </div>
+  </div>
+</template>
+
+<script>
+import { QSpinner } from 'quasar'
+import { mapGetters } from 'vuex'
+
+export default {
+  components: {
+    QSpinner
+  },
+  computed: mapGetters({
+    leftDrawerSize: 'layout/leftDrawerVisibleSize',
+    loading: 'layout/mapLoading'
+  })
+}
+</script>
+
+<style lang="stylus">
+.loader
+  position: absolute
+  background-color: rgba(0, 0, 0, 0.2)
+  top: 0
+  bottom: 0
+  left: 0
+  right: 0
+  z-index: 990 // Leaflet controls have z-index 1000
+  pointer-events: none // Everything under it continues working
+</style>
--- src/store/module-layout/getters.js
+++ src/store/module-layout/getters.js
@@ -2,13 +2,16 @@ export function drawersFullOverlay (state) {
   return state.drawerBehavior === 'mobile' || state.drawerBreakpoint >= state.size.width
 }
 
-export function hiddenMap (state, getters) {
+export function leftDrawerVisibleSize (state, getters) {
   const sidebarOverlaying = getters['drawersFullOverlay']
   const sidebarVisible = state.sidebarVisible
+  return (!sidebarOverlaying && sidebarVisible) ? state.leftDrawerSize : 0
+}
 
+export function hiddenMap (state, getters) {
   return {
     right: 0,
-    left: (!sidebarOverlaying && sidebarVisible) ? state.leftDrawerSize : 0,
+    left: getters['leftDrawerVisibleSize'],
     top: 0,
     bottom: 0
   }
@@ -16,4 +19,8 @@ export function hiddenMap (state, getters) {
 
 export function dialog (state) {
   return state.dialogs.length > 0 && state.dialogs[0]
+}
+
+export function mapLoading (state, getters, rootState, rootGetters) {
+  return rootGetters['dataLayer/mapLoading']
 }
