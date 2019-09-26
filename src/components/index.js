74456102f43b4d128ffc5df778bc4d8844871ca7
Martí Angelats i Ribera <ma@microdisseny.com>
2019-09-26 10:38:48 +0200

    Export utilities and components

---
+++ src/components/index.js
@@ -0,0 +1,35 @@
+import BasicPopup from './BasicPopup'
+import BooleanDialog from './BooleanDialog'
+import CopyToClipboard from './CopyToClipboard'
+import GeoportalMap from './GeoportalMap'
+import ImageDialog from './ImageDialog'
+import LoadingMapState from './LoadingMapState'
+import PopupData from './PopupData'
+import PrintDate from './PrintDate'
+import QueryOnClick from './QueryOnClick'
+import ResultPanelMixin from './ResultPanelMixin'
+import SearchBar from './SearchBar'
+import SearchResult from './SearchResult'
+import SearchResultPopup from './SearchResultPopup'
+import SearchResultPopupDialog from './SearchResultPopupDialog'
+import Sidebar from './Sidebar'
+import ValidatedInput from './ValidatedInput'
+
+export default {
+  BasicPopup,
+  BooleanDialog,
+  CopyToClipboard,
+  GeoportalMap,
+  ImageDialog,
+  LoadingMapState,
+  PopupData,
+  PrintDate,
+  QueryOnClick,
+  ResultPanelMixin,
+  SearchBar,
+  SearchResult,
+  SearchResultPopup,
+  SearchResultPopupDialog,
+  Sidebar,
+  ValidatedInput
+}
---
+++ src/lib/index.js
@@ -0,0 +1,23 @@
+import DottedPath from './DottedPath'
+import except from './except'
+import * as future from './FuturePromise'
+import * as geomUtils from './geomUtils'
+import initGmaps from './gmaps'
+import normalizeImage from './normalizeImage'
+import * as notifications from './notifications'
+import Options from './options'
+import * as utils from './utils'
+import L from './leaflet'
+
+export default {
+  DottedPath,
+  except,
+  future,
+  geomUtils,
+  initGmaps,
+  normalizeImage,
+  notifications,
+  Options,
+  utils,
+  L
+}
