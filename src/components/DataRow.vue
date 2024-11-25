c753ecaecd34b5040ec6bed0e05eab1acb8946d6
aroiginfraplan <59873981+aroiginfraplan@users.noreply.github.com>
2024-11-25 13:47:37 +0100

    Fix dataRow if is number

--- src/components/DataRow.vue
+++ src/components/DataRow.vue
@@ -42,7 +42,7 @@ export default {
       return !!this.value.includes && this.value.includes('<span')
     },
     isLink () {
-      return this.value.toLowerCase().startsWith('http')
+      return this.value.toLowerCase && this.value.toLowerCase().startsWith('http')
     },
     label () {
       return this.formatString(this.element.label)
