3a15ec68d82e7c7d0f618ba773a3e9dfc02b6bd5
Jordi Gomara Cruz <jg@microdisseny.com>
2024-11-28 15:09:01 +0100

    Update lib.js

--- src/lib.js
+++ src/lib.js
@@ -14,6 +14,7 @@ import DataRowToggle from 'components/DataRowToggle'
 import Sidebar from 'components/Sidebar'
 import DrawPanel from 'components/DrawPanel'
 import IncidencePanel from 'components/IncidencePanel'
+import PaletteSelect from 'components/statistics/PaletteSelect'
 import PlacePanel from 'components/PlacePanel'
 import PlacePanelStrategy from 'components/PlacePanelStrategy'
 import SearchBar from 'components/SearchBar'
@@ -55,6 +56,7 @@ export {
   Sidebar,
   DrawPanel,
   IncidencePanel,
+  PaletteSelect,
   PlacePanel,
   PlacePanelStrategy,
   SearchBar,
