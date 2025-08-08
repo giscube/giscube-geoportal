c8bf0aa002d5526d81d1aa6b8677d842fe115fe9
Jordi Gomara Cruz <jg@microdisseny.com>
2025-08-08 16:00:03 +0200

    f

--- src/components/OcupacioPanel.vue
+++ src/components/OcupacioPanel.vue
@@ -1,22 +1,83 @@
 <template>
-    <div class="ocupacio-panel">
-        <q-input v-model="beginDate" filled type="date" />
-        <q-input v-model="endDate" filled type="date" />
+  <div class="panel">
+    <div class="panel-content">
+      <div class="panel-title">Ocupació Via Pública</div>
+      <div>
+        <p>
+          Consulta les dades d'ocupació de la via pública al municipi de Sant Cugat del Vallès.
+        </p>
+        <p>
+          Selecciona el rang de dates que vols consultar i prem "Actualitza" per veure els talls al mapa.
+          Per consultar només un dia fes click dues vegades a la mateixa data.
+        </p>
+        <div style="padding: 12px; padding-top: 15px;!important padding-bottom: -5px;!important border-radius: 25px;">
+        </div>
+        <div v-show="isAuthenticated" class="q-pb-sm">
+          <div><q-toggle v-model="tallsExt" label="Talls (extern)" color="warning" keep-color /></div>
+          <div><q-toggle v-model="tallsInt" label="Talls (intern)" color="red" keep-color /></div>
+          <div><q-toggle v-model="assabentatsInt" label="Assabentats (intern)" color="blue" keep-color /></div>
+        </div>
+      </div>
+      <div>
+        <q-date v-model="selectedDates" range mask="DD/MM/YYYY"/>
+      </div>
+      <div class="q-py-sm">
+        <q-btn label="Actualitza" color="primary" @click="updateTalls" :loading="loading" :disable="!selectedDates">
+          <q-tooltip v-if="!selectedDates">
+            Selecciona una data
+          </q-tooltip>
+        </q-btn>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
-import { QInput } from 'quasar'
+import { QBtn, QDate, QTooltip, QToggle } from 'quasar'
 
 export default {
   name: 'OcupacioPanel',
   components: {
-    QInput
+    QBtn,
+    QDate,
+    QTooltip,
+    QToggle
+  },
+  beforeRouteEnter (to, from, next) {
+    next(vm => {
+      const today = new Date()
+      vm.selectedDates = today.toJSON().slice(0, 10).split('-').reverse().join('/')
+    })
   },
   data () {
     return {
-      beginDate: '2025-06-07',
-      endDate: '2025-06-07'
+      selectedDates: {
+        from: null,
+        to: null
+      }
+    }
+  },
+  computed: {
+    apiKey () {
+      return this.$store.state.ocupacio.token
+    },
+    loading () {
+      return this.$store.state.ocupacio && this.$store.state.ocupacio.loading
+    },
+    isAuthenticated () {
+      return this.$store.state.auth.accessToken
+    }
+  },
+  methods: {
+    updateOcupacio () {
+      const date1 = this.selectedDates.from || this.selectedDates
+      const date2 = this.selectedDates.to || this.selectedDates
+      this.$store.dispatch('obres/updateOcupacio', {
+        url: 'https://aupacaux.apps.santcugat.cat/api/GetOvpGeomByDates?geoque=LINES',
+        apiKey: this.apiKey,
+        date1,
+        date2
+      })
     }
   }
 }
